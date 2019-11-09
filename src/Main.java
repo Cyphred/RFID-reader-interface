4c6326ed1b609a16e090b292f6f3a8596e65430e
Jeremy Zantua <jeremyzantua@gmail.com>
2019-11-09 16:07:50 +0800

    Added function for creating new PINs

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,15 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         int menuState = 0;
 
-        while (menuState != -1) {
+        while (menuState != 100) {
             if (menuState == 0) {
                 System.out.println("[MENU]");
                 System.out.println("1 - Basic Scan");
                 System.out.println("2 - Challenge");
+                System.out.println("3 - New Passcode");
 
+                System.out.println("\n100 - Exit");
+                System.out.print("Input : ");
                 menuState = Integer.parseInt(sc.nextLine());
             }
             else if (menuState == 1) {
@@ -32,10 +35,17 @@ public class Main {
                 }
                 menuState = 0;
             }
+            else if (menuState == 3) {
+                System.out.println("Waiting for a passcode...");
+                System.out.println("Passcode Received : " + device.newPasscode());
+                menuState = 0;
+            }
             else {
                 System.out.println("Invalid Menu Option");
                 menuState = 0;
             }
         }
+        System.out.println("Exiting...");
+        System.exit(0);
     }
 }
