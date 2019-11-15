573302c181c3f1f2a51e99e0a9d0c02133d8574f
Jeremy Zantua <jeremyzantua@gmail.com>
2019-11-15 22:51:01 +0800

    Separated Scan and return UID functions

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,14 @@ public class Main {
                 System.out.println("1 - Basic Scan");
                 System.out.println("2 - Challenge");
                 System.out.println("3 - New Passcode");
+                System.out.println("4 - Print Last Scanned Card");
 
                 System.out.println("\n100 - Exit");
                 System.out.print("Input : ");
                 menuState = Integer.parseInt(sc.nextLine());
             }
             else if (menuState == 1) {
-                String serialNumber = device.scan();
-                device.clearLastStringRead();
-                System.out.println("[Basic Scan]");
-                System.out.println("Serial Number: " + serialNumber);
+                device.scan();
                 menuState = 0;
             }
             else if (menuState == 2) {
@@ -38,6 +36,10 @@ public class Main {
             else if (menuState == 3) {
                 System.out.println("Waiting for a passcode...");
                 System.out.println("Passcode Received : " + device.newPasscode());
+                menuState = 0;
+            }
+            else if (menuState == 4) {
+                System.out.println("SN: " + device.getLastScannedIDSerialNumber());
                 menuState = 0;
             }
             else {
